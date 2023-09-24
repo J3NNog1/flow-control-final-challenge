81f8f6304abdd7c81290c14fe51c4ef5295dfacf
Jenn <jenniferjuarezjj@gmail.com>
2023-09-24 18:52:07 -0400

    input two number expression

--- FlowControlChallenge.java
+++ FlowControlChallenge.java
@@ -4,7 +4,11 @@ public class FlowControlChallenge {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-
+//1. input two numbers
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
 
 
     scanner.close();
