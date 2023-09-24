2b3af016c592b0b8fb121fef3749798718b8a4af
Jenn <jenniferjuarezjj@gmail.com>
2023-09-24 18:50:56 -0400

    import scanner/ add scanner functionality/ close scanner

--- FlowControlChallenge.java
+++ FlowControlChallenge.java
@@ -1,7 +1,13 @@
 
-
+import java.util.Scanner;
 public class FlowControlChallenge {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+
+
+
+    scanner.close();
 
     }
 }
