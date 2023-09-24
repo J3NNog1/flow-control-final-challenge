9325b273c3691b5602c1bbd11d9fba7d49196913
Jenn <jenniferjuarezjj@gmail.com>
2023-09-24 19:42:51 -0400

    declaring variables

--- FlowControlChallenge.java
+++ FlowControlChallenge.java
@@ -2,6 +2,12 @@
 import java.util.Scanner;
 public class FlowControlChallenge {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        //below i am declaring var. num1 & num 2 are integer variables
+        //evenDivBy4Sum/8/notEvenNotDivBy5Sum are integer vars to store the sum of differ types of numbers
+        int num1, num2;
+        int evenDivBy4Sum = 0;
+        int evenDivBy8Sum = 0;
+        int notEvenNotDivBy5Sum = 0;
     }
 }
